c3af16f79c9a8579b53c74764f84ea60a123d68d
Bertrand Halaby <halabb11@gmail.com>
2021-11-29 22:45:16 +0000

    added rock paper scissors

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -4,6 +4,21 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def display_results (player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+    (player == 'paper' && computer == 'rock') ||
+    (player == 'scissors' && computer == 'paper')
+      prompt("You Won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+          (player == 'paper' && computer == 'scissors') ||
+          (player == 'scissors' && computer == 'rock')
+      prompt("You lost!")
+  else
+    prompt("It's a tie")
+  end
+
+end
+
 choice = ''
 loop do 
   prompt("Choose one: #{VALID_CHOICES.join (', ')}")
@@ -16,8 +31,9 @@ loop do
   end
   
 end
-
-
 computer_choice = VALID_CHOICES.sample
+
+display_results(choice, computer_choice)
+
 
 prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
