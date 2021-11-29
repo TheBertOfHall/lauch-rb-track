3aa601acfc3eea7dc1e244791980a9b55d77fd6a
Bertrand Halaby <halabb11@gmail.com>
2021-11-29 22:57:46 +0000

    finished rock paper scissors

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -4,14 +4,16 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def display_results (player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-    (player == 'paper' && computer == 'rock') ||
-    (player == 'scissors' && computer == 'paper')
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
       prompt("You Won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-          (player == 'paper' && computer == 'scissors') ||
-          (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
       prompt("You lost!")
   else
     prompt("It's a tie")
@@ -19,21 +21,25 @@ def display_results (player, computer)
 
 end
 
-choice = ''
-loop do 
-  prompt("Choose one: #{VALID_CHOICES.join (', ')}")
-  choice = gets.chomp 
-  
-  if VALID_CHOICES.include?(choice)
-    break
-  else 
-    prompt("That's not a valid choice.")
+loop do
+  choice = ''
+  loop do 
+    prompt("Choose one: #{VALID_CHOICES.join (', ')}")
+    choice = gets.chomp 
+    
+    if VALID_CHOICES.include?(choice)
+      break
+    else 
+      prompt("That's not a valid choice.")
+    end
+    
   end
+  computer_choice = VALID_CHOICES.sample
   
+  display_results(choice, computer_choice)
+  
+  
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 end
-computer_choice = VALID_CHOICES.sample
 
-display_results(choice, computer_choice)
-
-
-prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+prompt("Thank you for playing!")
